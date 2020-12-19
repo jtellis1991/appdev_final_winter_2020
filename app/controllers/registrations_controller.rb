0da80f261061a2d4d2cd33b2e43e3d4ce32720aa
Josh Ellis <josh@satsucceed.com>
2020-12-19 22:53:00 +0000

    Add new action to Registrations Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,40 @@
 class RegistrationsController < Devise::RegistrationsController
+  # prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
+  # prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy, :show]
+
+  # GET /resource/sign_up
+  def new
+    super
+  end
+
+  # # POST /resource
+  # def create
+  #   super
+  # end
+
+  # # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # def update
+  #   super
+  # end
+
+  # # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # def show
+  # end
 
 
+  # protected
 
+  #   def after_sign_up_path_for(student)
+  #     after_sign_in_path_for(student)
+  #   end
 
-  private
-    
-    def sign_up_params
-      params.require(:student).permit(:name, :email, :password, 
-                                   :password_confirmation)
-    end
-    
-    def account_update_params
-      params.require(:student).permit(:name, :email, :password, 
-                                      :password_confirmation, :current_password)
-    end                                      
+end                                    
 
-end
