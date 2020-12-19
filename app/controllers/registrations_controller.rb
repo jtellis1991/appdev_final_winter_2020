9ba353c4679f3bd337022256f09844441f8cc71d
Josh Ellis <josh@satsucceed.com>
2020-12-19 23:10:07 +0000

    Uncommented Regsitrations Controller, but left empty

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-# class Students:RegistrationsController < Devise::RegistrationsController
+class Students:RegistrationsController < Devise::RegistrationsController
   # prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
   # prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy, :show]
 
@@ -36,5 +36,5 @@
   #     after_sign_in_path_for(student)
   #   end
 
-# end                                    
+end                                    
 
