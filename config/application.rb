46c9919157adcf5991849396032d2e6a4656b667
Josh Ellis <josh@satsucceed.com>
2020-12-22 20:01:48 +0000

    add middleware for cloudflareproxy

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative "boot"
 
 require "rails/all"
+require "app/middleware/customware"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +20,7 @@ module SATApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.middleware.use CloudflareProxy
   end
 end
