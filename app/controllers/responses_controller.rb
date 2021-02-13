13c4aaefab408aa9fd9b28de61d64fda0d9c641b
Josh Ellis <josh@satsucceed.com>
2021-02-13 02:02:17 +0000

    completed the build of the calculator, realized I need to update the question model in order to display the calculator input of a response. about to make that change

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -6,21 +6,41 @@ class ResponsesController < ApplicationController
   @user = User.find(params[:user][:id])
   @question = Question.find(params[:question][:id])
   @test = @question.test
-  @options = @question.options.where(:value => params[:option])
+  @style = @question.style
   
-  @options.each do |option|
-    if option.correct == false
-      @response.correct = false
+  if @style.name == "math select one"
+  
+    @options = @question.options.where(:value => params[:option])
+    @options.each do |option|
+      if option.correct == false
+        @response.correct = false
+      end 
     end 
-  end 
-  if @response.correct.nil?
-    @response.correct = true
+    if @response.correct.nil?
+      @response.correct = true
+    end
+    @response.options = @options
+      
+  else
+    @answers = @question.answers
+    text = params["first col"] + params["second col"] + params["third col"] + params["fourth col"]
+    @response.text_submitted = text
+    @response.correct = nil
+    @answers.each do |answer|
+      if answer.correct_answer.gsub(/[^a-z0-9$.\/]/, "") == @response.text_submitted.gsub(/[^a-z0-9$.\/]/, "") 
+        @response.correct = true
+      end
+    end
+    if @response.correct.nil?
+      @response.correct = false
+    end
   end
+  
   @response.milliseconds_elapsed = params[:milliseconds]
   @response.question = @question
   @response.test_attempt = @test_attempt
   @response.user = @user
-  @response.options = @options
+
   
   @response.save!
   
@@ -38,19 +58,62 @@ class ResponsesController < ApplicationController
   @test_attempt = TestAttempt.find(params[:test_attempt][:id])
   @question = Question.find(params[:question][:id])
   @test = @question.test
-  @options = @question.options.where(:value => params[:option])
+  @style = @question.style
   
-  @response.correct = nil
-  @options.each do |option|
-    if option.correct == false
-      @response.correct = false
+  if @style.name == "math select one"
+    @options = @question.options.where(:value => params[:option])
+    @response.correct = nil
+    @options.each do |option|
+      if option.correct == false
+        @response.correct = false
+      end 
     end 
-  end 
-  if @response.correct.nil?
-    @response.correct = true
+    if @response.correct.nil?
+      @response.correct = true
+    end
+    @response.options = @options
+  else
+    @answers = @question.answers
+    if params["first col"].nil?
+      first_col = ""
+    else
+      first_col = params["first col"]
+    end 
+    if params["second col"].nil?
+      second_col = ""
+    else
+      second_col = params["second col"]
+    end 
+    if params["third col"].nil?
+      third_col = ""
+    else
+      third_col = params["third col"]
+    end 
+    if params["fourth col"].nil?
+      fourth_col = ""
+    else
+      fourth_col = params["fourth col"]
+    end 
+    text = first_col + second_col + third_col + fourth_col
+    @response.text_submitted = text
+    @response.correct = nil
+    @answers.each do |answer|
+      if answer.correct_answer.gsub(/[^a-z0-9$.\/]/, "") == @response.text_submitted.gsub(/[^a-z0-9$.\/]/, "") 
+        @response.correct = true
+      end
+    end
+    if @response.correct.nil?
+      @response.correct = false
+    elsif params["first col"].nil? && params["third col"].nil? && !params["second col"].nil? && !params["fourth col"].nil?
+      @response.correct = false
+    elsif !params["first col"].nil? && !params["third col"].nil? && params["second col"].nil? && params["fourth col"].nil?
+      @response.correct = false
+    elsif !params["first col"].nil? && params["third col"].nil? && params["second col"].nil? && !params["fourth col"].nil?
+      @response.correct = false
+    end
   end
+  
   @response.milliseconds_elapsed = params[:milliseconds]
-  @response.options = @options
   @response.save!
   
   @test.questions.each_with_index do |question, index|
