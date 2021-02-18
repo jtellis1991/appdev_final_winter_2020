e0947db8fa0584b89436341b14341de76cb6440e
Josh Ellis <josh@satsucceed.com>
2021-02-18 03:31:37 +0000

    stage activations for commit

---
+++ db/migrate/20210218033006_create_activations.rb
@@ -0,0 +1,13 @@
+class CreateActivations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :activations do |t|
+      t.integer :user_id
+      t.integer :strategy_id
+      
+      t.timestamps
+    end
+
+    add_foreign_key :activations, :users
+    add_foreign_key :activations, :strategies
+  end
+end
